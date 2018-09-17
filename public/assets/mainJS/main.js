cd096749440249652522fd08773bb5b6d9a5e4b4
Deller <dadeller@matw.com>
2018-09-17 16:14:51 -0400

    adding .babelrc and .gitignore

--- public/assets/mainJS/main.js
+++ public/assets/mainJS/main.js
@@ -1,15 +1,11 @@
-"use strict";
-
 // HOMEPAGE JS
 $(function () {
-  $('.burger').on('click', function () {
+  $('.burger').on('click', () => {
     $('.mobile-nav').addClass('active-nav');
   });
-  $('.close-nav').on('click', function () {
+  $('.close-nav').on('click', () => {
     $('.mobile-nav').removeClass('active-nav');
   });
 });
-"use strict";
-
 // CATALOG PAGE JS
 $(function () {});
